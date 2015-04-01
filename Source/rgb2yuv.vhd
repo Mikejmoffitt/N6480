30d4393192f6f3991b9a3dd2b8c3a06efc0926d1
Michael Moffitt <mikejmoffitt@gmail.com>
2015-03-31 20:33:43 -0400

    Made YUV conversion 10 bits

--- Source/rgb2yuv.vhd
+++ Source/rgb2yuv.vhd
@@ -8,13 +8,13 @@ use ieee.std_logic_unsigned.all;
 
 entity rgb2yuv is
 port (
-	r: in std_logic_vector(7 downto 0);
-	g: in std_logic_vector(7 downto 0);
-	b: in std_logic_vector(7 downto 0);
+	r: in std_logic_vector(9 downto 0);
+	g: in std_logic_vector(9 downto 0);
+	b: in std_logic_vector(9 downto 0);
 	
-	y: out std_logic_vector(7 downto 0);
-	u: out std_logic_vector(7 downto 0); -- AKA Cr
-	v: out std_logic_vector(7 downto 0); -- AKA Cb
+	y: out std_logic_vector(9 downto 0);
+	u: out std_logic_vector(9 downto 0); -- AKA Cr
+	v: out std_logic_vector(9 downto 0); -- AKA Cb
 	
 	sw_en: in std_logic -- Active high
 	
@@ -40,9 +40,9 @@ begin
 	make_yuv: process(r, g, b, sw_en)
 	begin
 		if (sw_en = '1') then
-			y <= ("00" & r(7 downto 2)) + ("00000" & r(7 downto 5)) + ("000000" & r(7 downto 6)) + ('0' & g(7 downto 1)) + ("0000" & g(7 downto 4)) + ("000000" & g(7 downto 6)) + ("000" & b(7 downto 3));
-			u <= 128 + ('0' & r(7 downto 1)) - (("00" & g(7 downto 2)) + ("000" & g(7 downto 3)) + ("00000" & g(7 downto 5)) + ("000000" & g(7 downto 6)))- (("0000" & b(7 downto 4)) + ("000000" & b(7 downto 6)));
-			v <= 128 + ('0' & b(7 downto 1))- (("00" & g(7 downto 2)) + ("0000" & g(7 downto 4)) + ("00000" & g(7 downto 5)))- (("000" & r(7 downto 3)) + ("00000" & r(7 downto 5)) + ("0000000" & r(7)));
+			y <= ("00" & r(9 downto 2)) + ("00000" & r(9 downto 5)) + ("000000" & r(9 downto 6)) + ("0" & g(9 downto 1)) + ("0000" & g(9 downto 4)) + ("000000" & g(9 downto 6)) + ("000" & b(9 downto 3));
+			u <= 512 + ("0" & r(9 downto 1)) - (("00" & g(9 downto 2)) + ("000" & g(9 downto 3)) + ("00000" & g(9 downto 5)) + ("000000" & g(9 downto 6)))- (("0000" & b(9 downto 4)) + ("000000" & b(9 downto 6)));
+			v <= 512 + ("0" & b(9 downto 1))- (("00" & g(9 downto 2)) + ("0000" & g(9 downto 4)) + ("00000" & g(9 downto 5)))- (("000" & r(9 downto 3)) + ("00000" & r(9 downto 5)) + ("0000000" & r(9)));
 		else
 			y <= r;
 			u <= g;
